7060470f9639a7a2dc8578880df542426b6b34bb
[Ernesto] <[ernesto.p@itmexicali.edu.mx]>
2018-09-05 19:50:40 -0700

    se agrego class cuadrado

--- Cuadrado.java
+++ Cuadrado.java
@@ -1,4 +1,11 @@
 package packageAllTest;
 
-public class Cuadrado {
+public class Cuadrado implements MethodsFigura {
+    double x;
+    @Override
+    public double calcularArea(double x) {
+        this.x=x;
+        return x;
+    }
+
 }
